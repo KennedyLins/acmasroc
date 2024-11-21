8ee105bb45453893e5ec7c42df211a6648948986
rnbruno <brunosmatias@gmail.com>
2024-11-21 23:27:38 +0000

    tarefa 2 2027 21-11

--- resources/views/site/component/section/container_interative.blade.php
+++ resources/views/site/component/section/container_interative.blade.php
@@ -1,59 +1,58 @@
 <section aria-label="section" class="no-padding bg-color home-only" style="background-size: cover;">
-    <div class="container-fluid" style="background-size: cover;">
+    <div class="container-fluid max-acm-box" style="background-size: cover;">
         <div class="d-lg-flex justify-content-around d-md-none" style="background-size: cover;">
-            <div class="feature-box pa-text f-boxed style-3 text-center gradient-light-1"
-                style="background-size: cover; width: 256px;">
+            <div class="feature-box feature-box-fix pa-text f-boxed style-3 text-center gradient-light-1"
+                style="background-size: cover;">
                 <i class="id-color "></i>
                 <div class="text" style="background-size: cover;">
                     <h4 class="text-responsive">Auditoria</h4>
-                    <p class="text-responsive">Análise de contas e documentos internos para empresas e
-                        outras e...</p>
+                    <p class="text-responsive text-wrap">Análise de contas e documentos internos para empresas e
+                        outras e... &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>
                 </div>
                 <i class="wm "></i>
                 <div class="spacer-single" style="background-size: cover;"></div>
                 <a href="auditoria.html" class="btn-custom_alter">Saber mais</a>
             </div>
-            <div class="feature-box pa-text f-boxed style-3 text-center gradient-light-2"
-                style="background-size: cover; width: 256px;">
+            <div class="feature-box feature-box-fix pa-text f-boxed style-3 text-center gradient-light-2"
+                style="background-size: cover;">
                 <i class="id-color "></i>
                 <div class="text" style="background-size: cover;">
                     <h4 class="text-responsive">Fiscalidade</h4>
-                    <p class="text-responsive">Solução ao nível do aconselhamento, de gestão de riscos, do
-                        ...</p>
+                    <p class="text-responsive text-wrap">Solução ao nível do aconselhamento, de gestão de riscos, do
+                        ... &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>
                 </div>
                 <i class="wm "></i>
                 <div class="spacer-single" style="background-size: cover;"></div>
                 <a href="fiscalidade.html" class="btn-custom_alter">Saber mais</a>
             </div>
-            <div class="feature-box pa-text f-boxed style-3 text-center gradient-light-3"
-                style="background-size: cover; width: 256px;">
+            <div class="feature-box feature-box-fix pa-text f-boxed style-3 text-center gradient-light-3"
+                style="background-size: cover;">
                 <i class="id-color "></i>
                 <div class="text" style="background-size: cover;">
                     <h4 class="text-responsive">Avaliação de empresas</h4>
-                    <p class="text-responsive">Apoio especializado em processos de ... </p>
+                    <p class="text-responsive text-wrap">Apoio especializado em processos de ... &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>
                 </div>
                 <i class="wm "></i>
                 <div class="spacer-single" style="background-size: cover;"></div>
                 <a href="avaliacao.html" class="btn-custom_alter">Saber mais</a>
             </div>
-            <div class="feature-box pa-text f-boxed style-3 text-center gradient-light-4"
-                style="background-size: cover; width: 256px;">
+            <div class="feature-box feature-box-fix pa-text f-boxed style-3 text-center gradient-light-4"
+                style="background-size: cover; ">
                 <i class="id-color "></i>
                 <div class="text" style="background-size: cover;">
                     <h4 class="text-responsive">Formação</h4>
-                    <p class="text-responsive">Num cenário de constante mudança, mantemos atualizados os
-                        profi...</p>
+                    <p class="text-responsive text-wrap">... &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>
                 </div>
                 <i class="wm "></i>
                 <div class="spacer-single" style="background-size: cover;"></div>
                 <a href="formacao.html" class="btn-custom_alter">Saber mais</a>
             </div>
-            <div class="feature-box pa-text f-boxed style-3 text-center gradient-light-5"
-                style="background-size: cover; width: 256px;">
+            <div class="feature-box feature-box-fix pa-text f-boxed style-3 text-center gradient-light-5"
+                style="background-size: cover; ">
                 <i class="id-color "></i>
                 <div class="text" style="background-size: cover;">
                     <h4 class="text-responsive">Consultoria e Reporting</h4>
-                    <p class="text-responsive">Acompanhando as necessidades mais variadas...</p>
+                    <p class="text-responsive text-wrap">Acompanhando as necessidades mais variadaompanhando as necessidades mais variadaompanhando as necessidades mais variadaompanhando as necessidades mais variadaompanhando as necessidades mais variadaompanhando as necessidades mais variadas... &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>
                 </div>
                 <i class="wm "></i>
                 <div class="spacer-single" style="background-size: cover;"></div>
